2fa0558b4300abd4860f03794243d06e8f9b33c8
Sidibe <Elstevehcks@gmail.com>
2022-05-30 15:45:38 -0400

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Kenneth Sidibe on 2022-05-30.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
